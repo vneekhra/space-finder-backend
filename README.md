55ff8a6ed60184b6104532cd1be3eb3d8db9cd4f
vneekhra <vikasnikhra@gmail.com>
2022-12-04 19:30:15 +0530

    base code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # space-finder-backend
+
+npm init -y
+npm i -D aws-cdk aws-cdk-lib constructs ts-node typescript
+tsc --init
+npm install --save-dev esbuild@0
+npm i uuid
+npm i @types/uuid
---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,36 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { GenericTable } from "./GenericTable";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { handler } from "../services/node-lambda/hello";
+
+export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+  private spacesTable = new GenericTable("SpacesTable", "spaceId", this);
+
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
+      handler: "hello.main",
+    });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
+      entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+      handler: "handler",
+    });
+
+    const helloLambdaIntegraion = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource("hello");
+    helloLambdaResource.addMethod("GET", helloLambdaIntegraion);
+  }
+}
